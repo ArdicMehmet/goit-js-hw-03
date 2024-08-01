fb230a8d6ebe5b03dfb0c93388bdedb8a74176d8
Mehmet Ardic <ardicmehmet96@gmail.com>
2024-08-01 11:43:07 +0300

    done homework3

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+function makeArray(firstArray, secondArray, maxLenght){
+    return [...firstArray, ...secondArray].slice(0, maxLenght); // maxLenght dizi boyutunu geçse de en fazla diziyi döndürüyor, bundan dolayı kontrol koymadım.
+}
+console.log("----------- Gorev 2 -------------");
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
